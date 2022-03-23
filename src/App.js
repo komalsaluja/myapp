8a51518029e1d61f5b648cf7b46d4d373c3c4191
komalsaluja <ks5908491@gmail.com>
2022-03-23 17:01:51 +0530

    ReactJSTutorial-DarkMode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import TextForm from './components/TextForm';
-//import About from './components/About';
+// import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+
 
 function App() {
+const [mode, setMode] = useState('light'); //whether dark mode is enabled or not    
+const [alert, setAlert] = useState(null)
+
+const showAlert = (message, type) => {
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=> {
+      setAlert(null);
+    }, 1500);
+}
+
+const toggleMode = () => {
+  if(mode === 'light')
+  {
+    setMode('dark');
+    document.body.style.backgroundColor = '#0e0230';
+   showAlert("Dark mode enabled", "success");
+   document.title = "TextUtils - DarkMode";
+    
+  }
+  else
+  {
+    setMode('light')
+    document.body.style.backgroundColor = 'white';
+    showAlert("Light mode enabled", "success");
+    document.title = "TextUtils - LightMode";
+  }
+}
+
   return (
    <>  
-<Navbar title="TextUtils" home="Home" about="About-TextUtils"  />
+<Navbar title="TextUtils" home="Home" about="About-TextUtils" mode={mode} toggleMode={toggleMode} />
+
+<Alert alert={alert} />
 
 <div className="container my-3">
-<TextForm heading="Enter the text to analyze below"/>
+<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
 {/* <About/> */}
 </div>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,16 +8,19 @@ const handleUpClick = ()=>{
   // console.log("Uppercase was clicked"+text);
   let newText = text.toUpperCase();
   setText(newText);
+  props.showAlert("Converted to UpperCase", "success");
 }
 
 const handledownClick = ()=>  {
   let newText = text.toLowerCase();
   setText(newText);
+  props.showAlert("Converted to LowerCase", "success");
 }
 
 const handleClearClick= () => {
   let newText = "";
   setText(newText);
+  props.showAlert("Text Cleared", "success");
 }
 
 const TitleCase= () => {
@@ -27,17 +30,23 @@ words.forEach(element => {
   uppercaseword = uppercaseword+element.charAt(0).toUpperCase()+element.slice(1).toLowerCase()+" "
 });
 setText(uppercaseword)
+props.showAlert("Converted to TitleCase", "success");
 }
 
 const handleCopy= () => {
  var text = document.getElementById("myBox");
  text.select();
  document.execCommand('copy');
+ navigator.clipboard.writeText(text.value);
+ props.showAlert("Text Copied to clipboard", "success");
+ 
+
 }
 
 const handleExtraSpaces = () => {
   let newText = text.split(/[ ]+/);
   setText(newText.join(" "))
+  props.showAlert("Extra Spaces removed", "success");
 }
 
 
@@ -50,10 +59,10 @@ const handleOnChange= (event)=>{
   const [text, setText] = useState('Enter Text Here');
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='dark' ? 'white' : 'black'}}>
     <h1>{props.heading}</h1>
     <div className="mb-3">
-     <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+     <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark' ? 'grey' : 'white' , color:props.mode==='dark' ? 'white' : 'black'}}></textarea>
     </div> 
     
     <button type="button" className="btn btn-primary" style={{margin: 3}} onClick={handleUpClick}>Convert to Uppercase</button>
@@ -65,12 +74,12 @@ const handleOnChange= (event)=>{
 
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark' ? 'white' : 'black'}}>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes Read</p>
       <h1>Preview</h1>
-      <p>{text}</p> 
+      <p>{text.length>0?text:"Enter something in the text box above to preview it here"}</p> 
     </div>
     </>
      )
